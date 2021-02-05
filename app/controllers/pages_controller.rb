be1a2177969866c6cb5535e8c0a763362c056b6c
marcus <https://github.com/marcuspure>
2021-02-05 14:24:07 +0800

    conflict

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,11 @@ class PagesController < ApplicationController
   def home
 
   end
-  
+
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone, :website, :image)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   validates :isbn,   :isbn_format => true
   validates :isbn10, :isbn_format => { :with => :isbn10 }
   validates :isbn13, :isbn_format => { :with => :isbn13 }
+
 end
