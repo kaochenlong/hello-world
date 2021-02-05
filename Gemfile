7b2d34eaacc7d8848c20f046cf0aead4f92d59a7
marcus <https://github.com/marcuspure>
2021-02-05 15:00:35 +0800

    finish

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.2'
-
+gem 'date', '~> 0.0.1'
 gem 'rails', '~> 6.1.1'
 gem 'sqlite3', '~> 1.4'
 gem 'puma', '~> 5.0'
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,14 @@ class Book < ApplicationRecord
   validates :isbn10, :isbn_format => { :with => :isbn10 }
   validates :isbn13, :isbn_format => { :with => :isbn13 }
 
+
+  validates :publish_date, presence: true, if: :article_published?
+
+  def check_published
+    self.publish_date = Time.now if self.published
+  end
+
+  def article_published?
+    self.published
+  end  
 end
