e98995a34f306479a6b6485f162c6153050e286f
Lilith <li6253462@gmail.com>
2022-02-07 15:28:12 +0800

    add upload cover function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_064640) do
+ActiveRecord::Schema.define(version: 2022_02_07_071736) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_02_07_064640) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "isbn"
+    t.string "image"
   end
 
 end
