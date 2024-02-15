907fb10036de9f727d40062c6b055bad339af867
Adoraxuu <adoraxu01@gmail.com>
2024-02-15 15:59:22 +0800

    fetch(validates):新增欄位驗證

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
+  validates :title, presence: true
   validates :isbn, presence: true, uniqueness: true, format: { with: /\A\d{10}|\d{13}\z/, message: "正確格式為13碼 or 10碼" }
+  validates :publish_date, presence: true
+  has_one_attached :cover
 end
